6110006a39b1fd5d5bdf541dfb68281cb4cfff23
RedaFarissi <redaesskouni@gmail.com>
2023-07-27 18:35:41 +0100

    add packages used  to requirements

--- payments/views.py
+++ payments/views.py
@@ -12,6 +12,8 @@ def process_payment(request):
     
     host = request.get_host()
     
+    print(order.items.all())
+
     paypal_dict = {
         'business': settings.PAYPAL_RECEIVER_EMAIL,
         'amount': order.get_total_cost() ,
